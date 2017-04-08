9d076a63194241b5a2edb844760487e4fa76783a
lararm <lrmadeir@gmail.com>
2017-04-08 11:35:45 -0400

    Add files via upload

---
+++ dbtest2.py
@@ -0,0 +1,81 @@
+import re
+import sys
+import sqlite3
+import codecs
+		
+#db = Db(sqlite3.connect('dbtest2' + '.db'), Sql())
+		
+
+'''
+db = sqlite3.connect('dbtest1.db')
+cur = db.cursor()
+
+##add data file into database
+category = "Sports"
+with open("w2_.txt", "r") as sports:
+    lines = sports.readlines()
+
+for line in lines:
+  # Split the line on whitespace
+  data = line.split()
+  number = data[0]
+  word1 = data[1]
+  print data
+  cur.execute(INSERT INTO users(person_id, category, type)
+			VALUES(?,?,?), (number, category, word1))
+
+db.commit()
+db.close()	
+		
+category = "Sports"
+with open("w2_.txt", "r") as sports:
+    lines = sports.readlines()
+
+for line in lines:
+    # Split the line on whitespace
+    data = line.split()
+    number = data[0]
+    word1 = data[1]
+print "end"	'''
+############### CREATE DB ##########################
+#db = sqlite3.connect('dbtest13.db')
+#cursor = db.cursor()
+#cursor.execute('''
+#    CREATE TABLE word(word1 TEXT,
+#                       word2 TEXT,count INTEGER)''')
+#cursor.execute('''
+#    CREATE TABLE param(name TEXT,
+    #                    value INTEGER)''')
+
+					   
+#db.commit()
+#db.close()
+#################INSERT DB #########################
+db = sqlite3.connect('dbtest13.db')
+cursor = db.cursor()
+
+import codecs
+
+	
+with open("w2_.txt", "r") as sports:
+    lines = sports.readlines()
+	
+for line in lines:
+    # Split the line on whitespace
+    data = line.split()
+    number = data[0]
+    word1 = data[1]
+    word2= data[2]
+    name ='depth'
+    value = 2
+   #print word2
+    cursor.execute('''INSERT INTO word(word1,word2,count)
+		VALUES(?,?,?)''', (word1,word2,number))	
+		
+    cursor.execute('''INSERT INTO param(name,value)
+		VALUES(?,?)''', (name,value))	
+#print "end"	
+ 
+db.commit()
+db.close()
+	
